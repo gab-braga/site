b310122f9a6e09ea114561cc6ccfd1d7e1de6ad5
Macbook <macbook@Gabriel-Braga.local>
2023-04-01 14:22:10 -0300

    Updates

--- src/components/Title/index.jsx
+++
@@ -1,5 +0,0 @@
-import "./style.css";
-
-export default function Title({children}) {
-    return <h1 className="title-page">{children}</h1>
-}
--- src/screens/Blog/index.jsx
+++
@@ -1,9 +0,0 @@
-import Title from "../../components/Title";
-
-export default function Blog() {
-    return (
-        <div className="blog container">
-            <Title>Blog</Title>
-        </div>
-    );
-}
