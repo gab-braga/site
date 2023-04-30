2ae59fae8b6d257a556e0a80284b96228aae85ec
f-gabriel-braga <gabrielbraga0712@gmail.com>
2023-04-30 11:56:44 -0300

    Update articles

--- src/data/articles.js
+++ src/data/articles.js
@@ -31,7 +31,7 @@ export const articles = [
     {
         id: "e667e4f3-aabf-41fd-adaf-0d7a3210f7ae",
         author: "Gabriel Braga",
-        publishedIn: 1681990127668,
+        publishedIn: 1682866480067,
         title: "Quem é Gabriel Braga em 2023?",
         image: {
             url: colacaoImageI,
@@ -42,23 +42,29 @@ export const articles = [
                 Meu nome é Francisco Gabriel Braga do Nascimento, brasileiro, tenho 21 anos, moro na cidade de Tianguá - CE. 
             </p>),
             (<p key={2}>
-                Trabalho como Professor, em uma incrivel Editech brasileira, ministrando aulas de tecnologia, com foco em desenvolvimento web, no formato remoto. Sou um professor apaixonado pelo que ensino, as aulas que ministro tem como destaque algumas das principais tecnologias e frameworks do mercado atual, como Spring, Angular, Node.js e React.
+                Nascido em Guaraciaba do Norte - CE, onde vivi e cresci, estudei até o ensino médio. Durante minha infância fui uma criança curiosa e aventureira, sempre me questionava como os eletrônicos funcionavam. Achava fascinante as possibilidades que um computador podia oferecer. Buscava desmontar computadores e eletrônicos quebrados para conhecer o que havia dentro mesmo sem saber o nome de cada componente e o que cada um deles faziam, as vezes não sabia montar de volta. Com o tempo, minha vontade de entender a aprender cada vez mais sobre tecnologia crescia.
             </p>),
             (<p key={3}>
-                Sou diplomado técnico em informática pela <a href="https://www.linkedin.com/company/eeep-deputado-jose-maria-melo/" target="_blank">EEEP Deputado José Maria Melo</a>, o qual estudei durante os anos de 2017 a 2019. Nesse período tive o privilégio de ter sido aluno dos professores <a href="https://www.linkedin.com/in/willian-ribeiro/" target="_blank">Willian Ribeiro</a> e <a href="https://www.linkedin.com/in/henrique-alves-685a1777/" target="_blank">Henrique Alves</a> que foram agentes que contribuiram, fortemente, para meu desenvolvimento inicial em tecnolgia, dois grande profissionais que abraçaram o exercicio de professor o qual, hoje, também atuo.
+                Hoje sou diplomado técnico em informática, o qual estudei durante 2017 a 2019. Pude então aprender os fundamentos de informática, tanto no âmbito de arquitetura de computadores quanto no âmbito de desenvolvimento de aplicações. Nesse período tive o privilégio de ter sido aluno dos professores <a href="https://www.linkedin.com/in/willian-ribeiro/" target="_blank">Willian Ribeiro</a> e <a href="https://www.linkedin.com/in/henrique-alves-685a1777/" target="_blank">Henrique Alves</a> que foram agentes que contribuíram, fortemente, para meu desenvolvimento inicial em tecnologia, dois grandes profissionais que abraçaram o exercício de professor.
                 <figure key={4}>
                     <figcaption>Colação do curso técnico em informática</figcaption>
                     <img src={colacaoImageII} alt="Colação do curso técnico em informática" />
                 </figure>
             </p>),
-            (<p>
-                Em 2018 pude participar da Jornada Nacional de Foguetes que aconteceu na cidade de Barra do Piraí - RJ. A OBA (Olimpiada Brasileira de Antronomia e Astronatica) tem diversos projetos que aplicam os conceitos de fisica e anstronomia. Dentre dele destaco a MOBFOG (Mostra Brasileira de Foguetes), uma prova prática que inclui contruir um foguete com base em materiais reciclaveis e não metalicos alimentado por reações quimicas não poluentes. Após participar da primeira fase fui selecionado para fase nacional, onde pude conhecer participantes de todo o pais além de ouvir grandes nomes brasileiros que estão ligados a projetos de física como o Astronauta Masrcos Pontes.
-            </p>),
             (<p key={4}>
-                Atualmente, sou estudante de bacharelado em ciências da computação pelo Instituto Federal de Educação, Ciência e Tecnologia do Ceará Campus Tianguá, que se deu inicio em 2020 e tem previsão de término em 2025. Aqui tenho realizado trabalhos e pesquisas relacinados a artigos ciêntificos e aplicação de algoritmos. Além disso, venho sempre me apromirando com estudos externos para garantir um maior escopo de conhecimento e ampliar minha experiência.
+                Em 2018 pude participar da Jornada Nacional de Foguetes que aconteceu na cidade de Barra do Piraí - RJ. A OBA (Olimpíada Brasileira de Astronomia e Astronáutica) tem diversos projetos que aplicam os conceitos de física e astronomia. Dentre dele destaco a MOBFOG (Mostra Brasileira de Foguetes), uma prova prática que inclui construir um foguete com base em materiais recicláveis e não metálicos alimentado por reações químicas não poluentes. Após participar da primeira fase fui selecionado para fase nacional em Barra do Piraí, onde pude conhecer participantes de todo o país além de ouvir grandes nomes brasileiros que estão ligados a projetos de física como o Astronauta Marcos Pontes.
             </p>),
             (<p key={5}>
-                Para 2023 contiuo trabalhando e me dedicando para o meu desenvolvimento pessoal e profissional. Diariamente, busco aprender novos termos, novos conceitos novas ferramentas, conhecer novos profissionais. Busco também aperfeiçoar o que já sei, treinando e me desafiando em novos projetos e funcinalidades.
+                Atualmente, sou estudante de bacharelado em ciências da computação pelo Instituto Federal de Educação, Ciência e Tecnologia do Ceará em Tianguá, que se deu início em 2020 e tem previsão de término em 2025. Aqui tenho realizado trabalhos e pesquisas relacionados a artigos científicos e aplicação de algoritmos. Além disso, venho sempre me aprimorando com estudos externos para garantir um maior escopo de conhecimento e ampliar minha experiência.
+            </p>),
+            (<p key={6}>
+                Trabalho como Professor, em uma incrível Editech brasileira, ministrando aulas de tecnologia, com foco em desenvolvimento web, no formato remoto. Sou um professor apaixonado pelo que ensino, as aulas que ministro tem como destaque algumas das principais tecnologias e frameworks do mercado atual, como Spring, Angular, Node.js e React.
+            </p>),
+            (<p key={7}>
+                Tenho habilidades em algumas linguagens de programação, incluindo Java, JavaScript e TypeScript, bem como em frameworks e bibliotecas como Spring, Hibernate, Angular, React e Node.js. Também tenho conhecimento prático em bancos de dados como MySQL e Firebase. Eu já apliquei essas habilidades em vários projetos (veja meu <a href="https://f-gabriel-braga.github.io/site/#/portfolio" target="_blank">portfólio</a>), como o sistema de gerenciamento de vendas, plataforma de música e landing pages.
+            </p>),
+            (<p key={8}>
+                Para 2023 continuo trabalhando e me dedicando para o meu desenvolvimento pessoal e profissional. Diariamente, busco aprender novos termos, novos conceitos, novas ferramentas, conhecer profissionais. Busco também aperfeiçoar o que já sei, treinando e me desafiando em projetos e funcionalidades.
             </p>)
         ]
     }
