50554e6bc9007e96b73eb1944611fc62be404a83
F-Gabriel-Braga <gabrielbraga0712@gmail.com>
2023-08-09 11:28:00 -0300

    updated style

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -2,10 +2,14 @@ import Initial from "../../components/Initial";
 import Skills from "../../components/Skills";
 import Projects from "../../components/Projects";
 import Contact from "../../components/Contact";
+import { Helmet } from "react-helmet";
 
 export default function Home() {
     return (
         <>
+            <Helmet>
+                <title>Gabriel Braga</title>
+            </Helmet>
             <Initial />
             <Projects />
             <Skills />
