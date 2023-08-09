c020b63cb88e853b3b1815e9a9b7aadfbaa66cea
F-Gabriel-Braga <gabrielbraga0712@gmail.com>
2023-08-09 10:44:56 -0300

    updated intro

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,29 +1,14 @@
 import Initial from "../../components/Initial";
-import Presentation from "../../components/Presentation";
 import Skills from "../../components/Skills";
 import Projects from "../../components/Projects";
 import Contact from "../../components/Contact";
-import { Helmet } from "react-helmet";
 
 export default function Home() {
     return (
         <>
-            <Helmet>
-                <meta property="fb:app_id" content="911989253208059" />
-                <meta property="og:type" content="website" />
-                <meta property="og:title" content="Gabriel Braga - Desenvolvedor Fullstack" />
-                <meta property="og:description" content="Descrubra quem sou e conheça meus projetos." />
-                <meta property="og:image" content="https://f-gabriel-braga.github.io/site/images/banner.png" />
-                <meta property="og:image:width" content="1200" />
-                <meta property="og:image:height" content="630" />
-                <meta property="og:locale" content="pt_BR" />
-                <meta property="og:url" content="http://f-gabriel-braga.github.io/site" />
-                <meta property="og:site_name" content="Gabriel Braga" />
-            </Helmet>
             <Initial />
-            <Presentation />
-            <Skills />
             <Projects />
+            <Skills />
             <Contact />
         </>
     );
