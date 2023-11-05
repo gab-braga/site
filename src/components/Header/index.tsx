77340b577a9a11d89c4e381927a88c3b3b60fe42
f-gabriel-braga <gabrielbraga0712@gmail.com>
2023-11-05 16:31:46 -0300

    feat: header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,35 @@
-import React from "react";
 import { NavLink } from "react-router-dom";
 import "./style.css";
 
 export default function Header() {
-    return (
-        <header className="header">
-            <nav className="menu">
-                <NavLink to="/" className={({isActive}) => isActive ? "active item" : "item"}>Home</NavLink>
-                <NavLink to="/portfolio" className={({isActive}) => isActive ? "active item" : "item"}>Portfólio</NavLink>
-                <NavLink to="/blog" className={({isActive}) => isActive ? "active item" : "item"}>Blog</NavLink>
-            </nav>
-        </header>
-    );
+  return (
+    <header className="header">
+      <nav className="container menu">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? "active menu-item" : "menu-item"
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/portfolio"
+          className={({ isActive }) =>
+            isActive ? "active menu-item" : "menu-item"
+          }
+        >
+          Portfólio
+        </NavLink>
+        <NavLink
+          to="/blog"
+          className={({ isActive }) =>
+            isActive ? "active menu-item" : "menu-item"
+          }
+        >
+          Blog
+        </NavLink>
+      </nav>
+    </header>
+  );
 }
