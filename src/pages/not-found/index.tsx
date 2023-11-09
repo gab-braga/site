3bb705eae770b79be9cc85e0faa3f0ec60d7550f
F-Gabriel-Braga <gabrielbraga0712@gmail.com>
2023-11-09 13:29:51 -0300

    feat: animation in page not found

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
   return (
     <div className="container not-found">
       <h1>
-        <span>404</span> Not Found
+        <span>404</span> <strong id="not-found-message"></strong>
       </h1>
       <Link to="/" className="back">
         Home
