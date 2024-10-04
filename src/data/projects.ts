5fd6f24d53332ea4a4be56c2cc85c2d095d235c0
gab-braga <gabrielbraga0712@gmail.com>
2024-10-03 22:44:52 -0300

    feat: add new projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,165 +1,187 @@
-import image01 from "../assets/images/projects/img01.jpg";
-import image02 from "../assets/images/projects/img02.jpg";
-import image03 from "../assets/images/projects/img03.jpg";
-import image04 from "../assets/images/projects/img04.jpg";
-import image05 from "../assets/images/projects/img05.jpg";
-import image06 from "../assets/images/projects/img06.jpg";
-import image07 from "../assets/images/projects/img07.jpg";
-import image08 from "../assets/images/projects/img08.jpg";
-import image09 from "../assets/images/projects/img09.jpg";
-import image10 from "../assets/images/projects/img10.jpg";
-import image11 from "../assets/images/projects/img11.jpg";
-import image12 from "../assets/images/projects/img12.jpg";
-import image13 from "../assets/images/projects/img13.jpg";
-import image14 from "../assets/images/projects/img14.jpg";
-import image15 from "../assets/images/projects/img15.jpg";
-import image16 from "../assets/images/projects/img16.jpg";
-import image17 from "../assets/images/projects/img17.jpg";
-import image18 from "../assets/images/projects/img18.jpg";
+import cinedev from "../assets/images/projects/cinedev.jpg";
+import collaborators from "../assets/images/projects/collaborators.jpg";
+import detetiveSQL from "../assets/images/projects/detetive-sql.png";
+import ecoleta from "../assets/images/projects/ecoleta.png";
+import esports from "../assets/images/projects/esports.jpg";
+import fastFood from "../assets/images/projects/fast-food.jpg";
+import galaxy from "../assets/images/projects/galaxy.jpg";
+import geometric from "../assets/images/projects/geometric.jpg";
+import goService from "../assets/images/projects/go-service.jpg";
+import happy from "../assets/images/projects/happy.jpg";
+import idealChair from "../assets/images/projects/ideal-chair.jpg";
+import imageCloud from "../assets/images/projects/image-cloud.jpg";
+import marketplace from "../assets/images/projects/marketplace.jpg";
+import music from "../assets/images/projects/music.jpg";
+import nQueens from "../assets/images/projects/n-queens.png";
+import pacman from "../assets/images/projects/pacman.jpg";
+import pdv from "../assets/images/projects/pdv.jpg";
+import pedeLimpeza from "../assets/images/projects/pede-limpeza.jpg";
+import pokedex from "../assets/images/projects/pokedex.jpg";
+import quizJs from "../assets/images/projects/quiz-js.jpg";
+import terragro from "../assets/images/projects/terragro.jpg";
+import toDoList from "../assets/images/projects/to-do-list.png";
+import weather from "../assets/images/projects/weather.jpg";
 
 export const projects: any = [
   {
-    id: 1,
-    image: image18,
+    image: detetiveSQL,
+    name: "Detetive SQL",
+    urlRepo: "https://sql-detective.web.app/",
+    stacks: ["React", "Firebase"],
+    year: 2024,
+  },
+  {
+    image: toDoList,
+    name: "To-Do List",
+    urlRepo: "https://github.com/gab-braga/to-do-list",
+    stacks: ["React", "Firebase"],
+    year: 2024,
+  },
+  {
+    image: cinedev,
     name: "Cine Dev",
     urlRepo: "https://github.com/gab-braga/cine-dev",
     stacks: ["Angular", "Spring", "JWT"],
     year: 2024,
   },
   {
-    id: 2,
-    image: image01,
-    name: "Maketplace App",
+    image: marketplace,
+    name: "Marketplace App",
     urlRepo: "https://github.com/gab-braga/marketplace-app",
     stacks: ["React", "Tailwind CSS"],
     year: 2023,
   },
   {
-    id: 3,
-    image: image02,
+    image: pokedex,
     name: "Pokedex App",
     urlRepo: "https://github.com/gab-braga/pokedex-app",
     stacks: ["React", "Talwind CSS", "TS"],
     year: 2023,
   },
   {
-    id: 4,
-    image: image03,
+    image: idealChair,
     name: "Ideal Chair",
     urlRepo: "https://github.com/gab-braga/ideal-chair",
     stacks: ["Bootstrap", "JS"],
     year: 2023,
   },
   {
-    id: 5,
-    image: image04,
+    image: imageCloud,
+    name: "Image Cloud",
+    urlRepo: "https://github.com/gab-braga/image-cloud",
+    stacks: ["Java", "Azure"],
+    year: 2024,
+  },
+  {
+    image: weather,
     name: "Weather App",
     urlRepo: "https://github.com/gab-braga/weather-app",
     stacks: ["Angular", "TS"],
     year: 2023,
   },
   {
-    id: 6,
-    image: image05,
+    image: terragro,
     name: "Terragro",
     urlRepo: "https://github.com/gab-braga/terragro",
     stacks: ["React", "CSS"],
     year: 2023,
   },
   {
-    id: 7,
-    image: image06,
+    image: quizJs,
     name: "Quiz",
     urlRepo: "https://github.com/gab-braga/quiz-js",
     stacks: ["JS", "CSS"],
     year: 2023,
   },
   {
-    id: 8,
-    image: image10,
+    image: geometric,
     name: "Plane Geometric 2D",
     urlRepo: "https://github.com/gab-braga/plane-geometric-2d",
-    stacks: ["JS", "CSS"],
+    stacks: ["JS", "HTML", "CSS"],
     year: 2023,
   },
   {
-    id: 9,
-    image: image14,
-    name: "eSports",
-    urlRepo: "https://github.com/gab-braga/esports-nlw",
-    stacks: ["React Native", "TS"],
-    year: 2023,
+    image: nQueens,
+    name: "N Queens",
+    urlRepo: "https://github.com/gab-braga/n-queens",
+    stacks: ["JS", "HTML", "CSS"],
+    year: 2024,
   },
   {
-    id: 10,
-    image: image16,
+    image: pacman,
     name: "Pac-Man",
     urlRepo: "https://github.com/gab-braga/pacman",
     stacks: ["C", "C++"],
     year: 2023,
   },
   {
-    id: 11,
-    image: image12,
+    image: goService,
     name: "Go Service",
     urlRepo: "https://github.com/gab-braga/go-service",
     stacks: ["Spring", "JWT", "Java"],
     year: 2023,
   },
   {
-    id: 12,
-    image: image07,
+    image: galaxy,
     name: "Galaxy App",
     urlRepo: "https://github.com/gab-braga/galaxy-app",
     stacks: ["Angular", "TS"],
     year: 2022,
   },
   {
-    id: 13,
-    image: image13,
-    name: "Collaboratores App",
+    image: collaborators,
+    name: "Collaborators App",
     urlRepo: "https://github.com/gab-braga/collaborators-app",
     stacks: ["Angular", "Firebase", "TS"],
     year: 2022,
   },
   {
-    id: 14,
-    image: image08,
+    image: music,
     name: "Music Player",
     urlRepo: "https://github.com/gab-braga/music-player",
     stacks: ["CSS", "TS"],
     year: 2021,
   },
   {
-    id: 15,
-    image: image09,
+    image: pedeLimpeza,
     name: "Pede Limpeza",
     urlRepo: "https://github.com/gab-braga/pede-limpeza",
     stacks: ["JS", "CSS"],
     year: 2021,
   },
   {
-    id: 16,
-    image: image11,
+    image: fastFood,
     name: "Fast Food",
     urlRepo: "https://github.com/gab-braga/fast-food",
     stacks: ["Thymeleaf", "Spring", "Java"],
     year: 2021,
   },
   {
-    id: 17,
-    image: image15,
+    image: pdv,
+    name: "Ponto de Venda",
+    urlRepo: "https://github.com/gab-braga/ponto-de-venda",
+    stacks: ["Java", "MySQL", "JavaFx"],
+    year: 2021,
+  },
+  {
+    image: esports,
+    name: "eSports |  NLW",
+    urlRepo: "https://github.com/gab-braga/esports-nlw",
+    stacks: ["React Native", "TS"],
+    year: 2023,
+  },
+  {
+    image: happy,
     name: "Happy | NLW",
     urlRepo: "https://github.com/gab-braga/happy-nlw",
     stacks: ["Node.js", "Handlebars"],
     year: 2021,
   },
   {
-    id: 18,
-    image: image17,
-    name: "Ponto de Venda",
-    urlRepo: "https://github.com/gab-braga/ponto-de-venda",
-    stacks: ["Java", "MySQL", "JavaFx"],
+    image: ecoleta,
+    name: "Ecoleta |  NLW",
+    urlRepo: "https://github.com/gab-braga/ecoleta-nlw",
+    stacks: ["Node.js", "Nunjucks", "SQLite"],
     year: 2021,
   },
 ];
--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,6 +1,6 @@
-import { projects } from "../../data/projects";
 import "./style.css";
 import Card from "../../components/card";
+import { projects } from "../../data/projects";
 
 export default function Portfolio() {
   return (
@@ -8,8 +8,8 @@ export default function Portfolio() {
       <div className="projects">
         <h1>Portfólio</h1>
         <div className="projects-list">
-          {projects.map((proj: any) => (
-            <Card key={proj.id} {...proj} />
+          {projects.map((proj: any, idx: number) => (
+            <Card {...proj} key={idx} />
           ))}
         </div>
       </div>
